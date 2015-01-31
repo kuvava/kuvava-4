e8bd7b70c2e52a2ff0c8cf28c3b4a4b2831b76b4
Tomáš Urban <urbanovi@kuvava.cz>
2015-01-31 23:00:49 +0100

    Add click jquery for #madd and #add

--- www/js/my-4-0-1.js
+++ www/js/my-4-0-1.js
@@ -1,3 +1,26 @@
-$(function(){
-
+$(document).ready(function(){
+	$('#madd1').click(function(){
+			$('#madd2,#madd3').removeClass('act');
+			$(this).toggleClass('act');
+			$('#add2,#add3').hide();
+			$('#add1').toggle();
+			ah1();
+			return false;
+	});
+	$('#madd2').click(function(){
+			$('#madd1,#madd3').removeClass('act');
+			$(this).toggleClass('act');
+			$('#add1,#add3').hide();
+			$('#add2').toggle();
+			ah1();
+			return false;
+	});
+	$('#madd3').click(function(){
+			$('#madd1,#madd2').removeClass('act');
+			$(this).toggleClass('act');
+			$('#add1,#add2').hide();
+			$('#add3').toggle();
+			ah1();
+			return false;
+	});
 });
