3e07bb072fa4d30d8c86ea595eca07e975dee7f8
Tomáš Urban <urbanovi@kuvava.cz>
2015-02-16 10:27:33 +0100

    Complete error handling (czech error pages, ...)

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -23,15 +23,19 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
 	public function startup()
 	{
 		parent::startup();
-		\AntispamControl::register();
-		$this->params = $this->getParameters();
+		if ($this->name !== 'Error'){
+			\AntispamControl::register();
+			$this->params = $this->getParameters();
+		}
 	}
 	
 	public function beforeRender()
 	{
-		$this->template->stranka = $this->database->table('stranka')->where('presenter = ?', $this->name)->where('url1 = ?', $this->params['url1'])->where('url2 = ?', $this->params['url2'])->where('number1 = ?', $this->params['number1'])->limit(1)->fetch();
-		if (!$this->template->stranka){
-			$this->shootError();
+		if ($this->name !== 'Error'){
+			$this->template->stranka = $this->database->table('stranka')->where('presenter = ?', $this->name)->where('url1 = ?', $this->params['url1'])->where('url2 = ?', $this->params['url2'])->where('number1 = ?', $this->params['number1'])->limit(1)->fetch();
+			if (!$this->template->stranka){
+				$this->shootError();
+			}	
 		}
 	}
 
@@ -44,7 +48,7 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
 		$renderer->wrappers['control']['container'] = 'dd';
 	}
 	
-	protected function shootError($message = 'Omlouváme se, ale stránku nelze nalézt.<br>Kontaktujte prosím správce webu: urbanovi&#64;<!-- -->kuvava.cz<br>nebo si vyberte jiný obsah v menu.', $class = 'flash-red', $errorText = 'Odkazovaný obsah nelze nalézt.')
+	protected function shootError($message = 'Omlouváme se, ale stránku nelze nalézt.<br>Kontaktujte prosím správce webu: urbanovi&#64;<!-- -->kuvava.cz<br>nebo si vyberte jiný obsah v menu.', $class = 'fr', $errorText = 'Odkazovaný obsah nelze nalézt.')
 	{
 		$this->flashMessage($message,$class);
 		$this->error($errorText);
