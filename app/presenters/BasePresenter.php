e245da3a9a050ff2693863fe08c39d0468a97d27
Tomáš Urban <urbanovi@kuvava.cz>
2015-02-16 21:44:18 +0100

    Prepare comment form

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -3,7 +3,9 @@
 namespace App\Presenters;
 
 use Nette,
-	App\Model;
+	App\Model,
+	Nette\Application\UI\Form,
+	Nette\Utils\Html;
 
 
 /**
@@ -32,6 +34,7 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
 	public function beforeRender()
 	{
 		if ($this->name !== 'Error'){
+			$this->template->params = $this->params;
 			$this->template->stranka = $this->database->table('stranka')->where('presenter = ?', $this->name)->where('url1 = ?', $this->params['url1'])->where('url2 = ?', $this->params['url2'])->where('number1 = ?', $this->params['number1'])->limit(1)->fetch();
 			if (!$this->template->stranka){
 				$this->shootError();
@@ -52,5 +55,63 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
 	{
 		$this->flashMessage($message,$class);
 		$this->error($errorText);
+	}
+	
+	public function createComponentVlozitKomentForm()
+	{
+		$form = new Nette\Application\UI\Form;
+			
+		$form->addText('prezdivka', 'Přezdívka:', 30, 30);
+		$form->addText('web', 'Vaše webové stránky:', 30, 50)
+			->setOption('description','(zobrazí se vedle přezdívky)');
+		$form->addTextArea('text', 'Text*:', 50, 20)
+			->setAttribute('maxlength', '2000')
+			->addRule(Form::FILLED, 'Vyplňte prosím políčko "text".');
+		$form->addText('email', 'Email*:', 30, 70)
+			->setType('email')
+			->addRule(Form::FILLED, 'Zadejte prosím Váš e-mail.')
+			->addRule(Form::EMAIL, 'Zadejte prosím platnou e-mailovou adresu.')
+			->setDefaultValue('@')
+			->setOption('description','(nezobrazí se ostatním)');
+		$form->addCheckbox('novinky', 'Smíme Vám zasílat novinky emailem?')
+			->setDefaultValue(FALSE);
+		$form->addAntispam();
+		$form->addSubmit('odeslat', 'Vložit komentář')
+			->setAttribute('class','but')
+			->setOption('description', Html::el('div class=des')
+					->setHtml('<small>(Nesouvisející dotazy pokládejte prosím v našem diskuzním fóru.)<br>(Text nelze formátovat. Odkazy převedeme ručně na klikatelné - zpravidla do 5 dnů.)<br>(Znění dle svého svědomí upravujeme. S autorem o tom komunikujeme emailem. Jde nám o výpovědní hodnotu i pro budoucí čtenáře webu. U pozměněných komentářů je vždy proklik i na původní podobu.)<br>(Údaje s * jsou povinné.)</small>')
+					);
+		$this->setFormRenderer($form);
+		
+		$form->onSuccess[] = array($this, 'napisteNamFormSubmitted');
+		return $form;
+	}
+	
+	public function vlozitKomentFormSubmitted(Nette\Application\UI\Form $form, $values)
+	{
+		$novinky = $values->novinky ? 'ano' : 'ne';
+		$zavolame_vam = $values->zavolame_vam ? 'ano' : 'ne';
+		$kopie = $values->kopie ? 'ano' : 'ne';
+		$httpRequest = $this->context->getService('httpRequest');
+		$now = new Nette\DateTime;
+		$ip = inet_pton($httpRequest->getRemoteAddress());
+
+		$data = array (
+			'jmeno' => $values->jmeno,
+			'telefon' => $values->telefon,
+			'zavolame_vam' => $zavolame_vam,
+			'email' => $values->email,
+			'novinky' => $novinky,
+			'text' => $values->text,
+			'kopie' => $kopie,
+			'dv' => $now,
+			'ov' => $now,
+			'ip' => $ip
+			);
+			
+		$this->database->table('koment')->insert($data);
+
+		$this->flashMessage('Váš vzkaz byl úspěšně odeslán na náš email: urbanovi&#64;<!-- -->kuvava.cz '. ($data['kopie'] === 'ano' ? ('<br>Pro kontrolu jsme kopii odeslali i na Váš email: '. $data['email']):''));
+		$this->redirect('this');
 	}
 }
