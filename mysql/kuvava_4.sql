a34f7a8e66f762e1e929c7f49b9e99eaffd7ef85
Tomáš Urban <urbanovi@kuvava.cz>
2015-02-19 01:22:35 +0100

    Some changes and new table in database

--- mysql/kuvava_4.sql
+++ mysql/kuvava_4.sql
@@ -9,6 +9,7 @@ DROP TABLE IF EXISTS `koment`;
 CREATE TABLE `koment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `stranka_id` int(11) NOT NULL,
+  `stranka_number2` int(11) NOT NULL DEFAULT '0',
   `prezdivka` varchar(30) COLLATE utf8_czech_ci NOT NULL,
   `web` varchar(50) COLLATE utf8_czech_ci NOT NULL,
   `email` varchar(70) COLLATE utf8_czech_ci NOT NULL,
@@ -27,12 +28,13 @@ CREATE TABLE `koment` (
   CONSTRAINT `koment_ibfk_1` FOREIGN KEY (`stranka_id`) REFERENCES `stranka` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
-INSERT INTO `koment` (`id`, `stranka_id`, `prezdivka`, `web`, `email`, `novinky`, `text`, `puvodni_zneni`, `redakcni_poznamka`, `pohlavi`, `vek`, `dv`, `ip`, `upraveno`, `smazano`) VALUES
-(1,	1,	'Micina',	'nemám',	'neco@neco.cz',	'ne',	'No teda, není ten Váš web nějak příliš strohý pro oko?',	'Ty pičo, není ten Váš web nějak příliš strohý pro oko?',	'Původně sprosté zvolání jsme nahradili zvoláním citovým - avšak jemnějším.',	'f',	'25 až 30',	'2015-02-17 02:18:29',	UNHEX('7F000001'),	1,	0),
-(2,	1,	'Tomáš Urban',	'www.kuvava.cz',	'urbanovi@kuvava.cz',	'ano',	'Udělali jsme jej pro začátek maximálně jednoduše. Věříme, že tím vynikne především textový obsah. Do budoucna budeme graficky, funkčně i obsahově obohacovat dle spontánních zkušeností a vzájemné inspirace s čtenáři... :-)',	'Udělali jsme jej pro začátek maximálně jednoduše. Věříme, že tím vynikne především textový obsah. Do budoucna budeme graficky, funkčně i obsahově obohacovat dle spontánních zkušeností a vzájemné inspirace s čtenáři... :-)',	NULL,	'm',	'30 až 35',	'2015-02-17 02:21:08',	UNHEX('7F000001'),	0,	0),
-(3,	1,	'hlasatel pravé víry',	'www.prozrete.com',	'pan@xy.cz',	'ano',	'Všichni jste pomocníci ďábla. Přijměte Ježíše do svého srdce a proste za odpuštění. Měli byste se kát... Jinak skončíte v pekle a budete zažívat věčná muka! Tak je psáno v Písmu Svatém...',	'Všichni jste pomocníci ďábla. Přijměte Ježíše do svého srdce a proste za odpuštění. Měli byste se kát... Jinak skončíte v pekle a budete zažívat věčná muka! Tak je psáno v Písmu Svatém...',	'Ctíme osobní víru uživatele, který vložil tento komentář. Přijde nám však, že tento příspěvek se nedrží daného tématu a vlákna a u budoucích čtenářů by spíše rušil porozumění u probíhající diskuze.',	'h',	'80 nebo více',	'2015-02-17 03:39:00',	UNHEX('7F000001'),	0,	1),
-(4,	1,	'Jan Omáčka',	'www.kavarnausedmiveverek.eu',	'info@kavarnausedmiveverek.eu',	'ano',	'Mě se naopak ta grafická jednoduchost velmi líbí. Oceňuji, že má někdo stránky bez reklam a soustředí se spíše na obsah a diskuze, než na velikost loga a lesklé okraje... :-)',	'Mě se naopak ta grafická jednoduchost velmi líbí. Oceňuji, že má někdo stránky bez reklam a soustředí se spíše na obsah a diskuze, než na velikost loga a lesklé okraje... :-)',	NULL,	'm',	'-1',	'2015-02-17 03:50:37',	UNHEX('7F000001'),	0,	0),
-(5,	1,	'',	'',	'janka_urbanova@centrum.cz',	'ne',	'Odvedli jste dobrou práci',	'Odvedli jste dobrou práci',	NULL,	'h',	'-1',	'2015-02-17 04:05:45',	UNHEX('7F000001'),	0,	0);
+INSERT INTO `koment` (`id`, `stranka_id`, `stranka_number2`, `prezdivka`, `web`, `email`, `novinky`, `text`, `puvodni_zneni`, `redakcni_poznamka`, `pohlavi`, `vek`, `dv`, `ip`, `upraveno`, `smazano`) VALUES
+(1,	1,	0,	'Micina',	'nemám',	'neco@neco.cz',	'ne',	'No teda, není ten Váš web nějak příliš strohý pro oko?',	'Ty pičo, není ten Váš web nějak příliš strohý pro oko?',	'Původně sprosté zvolání jsme nahradili zvoláním citovým - avšak jemnějším.',	'f',	'25 až 30',	'2015-02-17 02:18:29',	UNHEX('7F000001'),	1,	0),
+(2,	1,	0,	'Tomáš Urban',	'www.kuvava.cz',	'urbanovi@kuvava.cz',	'ano',	'Udělali jsme jej pro začátek maximálně jednoduše. Věříme, že tím vynikne především textový obsah. Do budoucna budeme graficky, funkčně i obsahově obohacovat dle spontánních zkušeností a vzájemné inspirace s čtenáři... :-)',	'Udělali jsme jej pro začátek maximálně jednoduše. Věříme, že tím vynikne především textový obsah. Do budoucna budeme graficky, funkčně i obsahově obohacovat dle spontánních zkušeností a vzájemné inspirace s čtenáři... :-)',	NULL,	'm',	'30 až 35',	'2015-02-17 02:21:08',	UNHEX('7F000001'),	0,	0),
+(3,	1,	0,	'hlasatel pravé víry',	'www.prozrete.com',	'pan@xy.cz',	'ano',	'Všichni jste pomocníci ďábla. Přijměte Ježíše do svého srdce a proste za odpuštění. Měli byste se kát... Jinak skončíte v pekle a budete zažívat věčná muka! Tak je psáno v Písmu Svatém...',	'Všichni jste pomocníci ďábla. Přijměte Ježíše do svého srdce a proste za odpuštění. Měli byste se kát... Jinak skončíte v pekle a budete zažívat věčná muka! Tak je psáno v Písmu Svatém...',	'Ctíme osobní víru uživatele, který vložil tento komentář. Přijde nám však, že tento příspěvek se nedrží daného tématu a vlákna a u budoucích čtenářů by spíše rušil porozumění u probíhající diskuze.',	'h',	'80 nebo více',	'2015-02-17 03:39:00',	UNHEX('7F000001'),	0,	1),
+(4,	1,	0,	'Jan Omáčka',	'www.kavarnausedmiveverek.eu',	'info@kavarnausedmiveverek.eu',	'ano',	'Mě se naopak ta grafická jednoduchost velmi líbí. Oceňuji, že má někdo stránky bez reklam a soustředí se spíše na obsah a diskuze, než na velikost loga a lesklé okraje... :-)',	'Mě se naopak ta grafická jednoduchost velmi líbí. Oceňuji, že má někdo stránky bez reklam a soustředí se spíše na obsah a diskuze, než na velikost loga a lesklé okraje... :-)',	NULL,	'm',	'-1',	'2015-02-17 03:50:37',	UNHEX('7F000001'),	0,	0),
+(5,	1,	0,	'',	'',	'janka_urbanova@centrum.cz',	'ne',	'Odvedli jste dobrou práci',	'Odvedli jste dobrou práci',	NULL,	'h',	'-1',	'2015-02-17 04:05:45',	UNHEX('7F000001'),	0,	0),
+(6,	1,	0,	'',	'',	'neco@neco.cz',	'ne',	'asdf\n\nasdkflj\n\n\nasdfkjlasdf\nasdflkjasdf',	'asdf\n\nasdkflj\n\n\nasdfkjlasdf\nasdflkjasdf',	NULL,	'm',	'-1',	'2015-02-18 16:46:56',	UNHEX('7F000001'),	0,	0);
 
 DROP TABLE IF EXISTS `napiste_nam`;
 CREATE TABLE `napiste_nam` (
@@ -50,15 +52,30 @@ CREATE TABLE `napiste_nam` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 
+DROP TABLE IF EXISTS `odkaz`;
+CREATE TABLE `odkaz` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `href` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `title_attr` varchar(255) COLLATE utf8_czech_ci DEFAULT NULL,
+  `text` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `target` char(1) COLLATE utf8_czech_ci NOT NULL DEFAULT 's',
+  `nofollow` smallint(6) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+
 DROP TABLE IF EXISTS `stranka`;
 CREATE TABLE `stranka` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `presenter` varchar(100) COLLATE utf8_czech_ci NOT NULL,
+  `view` varchar(100) COLLATE utf8_czech_ci NOT NULL DEFAULT 'default',
   `url1` varchar(100) COLLATE utf8_czech_ci NOT NULL,
   `url2` varchar(100) COLLATE utf8_czech_ci NOT NULL,
   `number1` int(11) NOT NULL DEFAULT '0',
   `title_tag` varchar(100) COLLATE utf8_czech_ci NOT NULL,
+  `title_facebook` varchar(100) COLLATE utf8_czech_ci NOT NULL,
   `description` varchar(200) COLLATE utf8_czech_ci NOT NULL,
+  `h1` varchar(100) COLLATE utf8_czech_ci NOT NULL,
   `content` text COLLATE utf8_czech_ci,
   `vizte_tez` text COLLATE utf8_czech_ci,
   `diskuze` tinyint(3) unsigned NOT NULL DEFAULT '1',
@@ -70,8 +87,8 @@ CREATE TABLE `stranka` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
-INSERT INTO `stranka` (`id`, `presenter`, `url1`, `url2`, `number1`, `title_tag`, `description`, `content`, `vizte_tez`, `diskuze`, `sdilet`, `clanek`, `archiv`, `submenu`, `actmenu`) VALUES
-(1,	'Www',	'',	'',	0,	'Jana Urbanová a Tomáš Urban Vás vítají',	'Jana a Tomáš Urbanovi se věnují například léčitelství, poradenství, věštectví nebo umění. Něco dělají zdarma, něco za peníze.',	'<h1 n:block=title>Vítejte na www.kuvava.cz!</h1>\r\n<p>Jsme Jana a Tomáš Urbanovi. Věnujeme se například umění, léčitelství a poradenství. Něco děláme zdarma, něco za peníze...</p>\r\n<h2>Naše kontaktní údaje</h2>\r\n<dl>\r\n<dt>Telefon:</dt>\r\n<dd><strong>00420 / 777 790 528</strong></dd>\r\n<dt>Email:</dt>\r\n<dd><a href=\"mailto:urbanovi@kuvava.cz\">urbanovi@<!-- -->kuvava.cz</a></dd>\r\n<dt>České číslo účtu:</dt>\r\n<dd><strong>107-3720830217 /&nbsp;0100</strong> (u&nbsp;Komerční banky)<br><small>(Pokud uvedete jako variabilní symbol své mobilní telefonní číslo, zašleme Vám smskou potvrzení, že jsme platbu zdárně obdrželi.)</small></dd>\r\n<dt>Slovenské číslo účtu:</dt>\r\n<dd>18561447 / 6500 (u Poštové banky)</dd>\r\n</dl>',	'<p><a href=\"/\">Velmi zajímavý článek k věci</a></p>\r\n<p><a href=\"/\">Související služba s tematickým popiskem</a></p>\r\n<p><a href=\"/\">Další související služba</a></p>',	1,	1,	0,	0,	NULL,	'm1c1'),
-(2,	'Diskuze',	'',	'',	0,	'Vzájemné rozhovory komunity kolem www.kuvava.cz',	'Diskuze o lidech, pohodě a léčitelství... I na jiná témata. Účastní se i autoři webu: Jana Urbanová a Tomáš Urban',	'',	NULL,	1,	1,	0,	0,	NULL,	'm2c3');
+INSERT INTO `stranka` (`id`, `presenter`, `view`, `url1`, `url2`, `number1`, `title_tag`, `title_facebook`, `description`, `h1`, `content`, `vizte_tez`, `diskuze`, `sdilet`, `clanek`, `archiv`, `submenu`, `actmenu`) VALUES
+(1,	'Www',	'default',	'',	'',	0,	'Jana Urbanová a Tomáš Urban Vás vítají',	'Vítejte na webu o lidech, pohodě a léčitelství',	'Jana a Tomáš Urbanovi se věnují například léčitelství, poradenství, věštectví nebo umění. Něco dělají zdarma, něco za peníze.',	'Vítejte na webu o lidech, pohodě a léčitelství<br>(www.kuvava.cz)',	'<p>Jsme Jana a Tomáš Urbanovi. Věnujeme se například umění, alternativní medicíně a poradenství. Především však rozvíjíme spokojenost. Něco děláme zdarma, něco za peníze...</p>\r\n<h2>Naše kontaktní údaje</h2>\r\n<dl>\r\n<dt>Telefon:</dt>\r\n<dd><strong>00420 / 777 790 528</strong></dd>\r\n<dt>Email:</dt>\r\n<dd><a href=\"mailto:urbanovi@kuvava.cz\">urbanovi@<!-- -->kuvava.cz</a></dd>\r\n<dt>České číslo účtu:</dt>\r\n<dd><strong>107-3720830217 /&nbsp;0100</strong> (u&nbsp;Komerční banky)<br><small>(Pokud uvedete jako variabilní symbol své mobilní telefonní číslo, zašleme Vám smskou potvrzení, že jsme platbu zdárně obdrželi.)</small></dd>\r\n<dt>Slovenské číslo účtu:</dt>\r\n<dd>18561447 / 6500 (u Poštové banky)</dd>\r\n</dl>',	'<p><a href=\"/\">Velmi zajímavý článek k věci</a></p>\r\n<p><a href=\"/\">Související služba s tematickým popiskem</a></p>\r\n<p><a href=\"/\">Další související služba</a></p>',	1,	1,	0,	0,	NULL,	'm1c1'),
+(2,	'Diskuze',	'puvodniZneni',	'',	'',	0,	'Vzájemné rozhovory komunity kolem www.kuvava.cz',	'',	'Diskuze o lidech, pohodě a léčitelství... I na jiná témata. Účastní se i autoři webu: Jana Urbanová a Tomáš Urban',	'',	'',	NULL,	2,	1,	0,	0,	NULL,	'm2c3');
 
--- 2015-02-17 04:14:36
+-- 2015-02-19 00:21:12
